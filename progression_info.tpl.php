1457cf04db1a1d98ee62be0e8f3e34a024729334
Gavin Mulligan <d3r1v3d@gmail.com>
2010-08-03 21:50:32 -0600

    Added a description field to counters.

--- progression_info.tpl.php
+++ progression_info.tpl.php
@@ -1,9 +1,15 @@
+<?php
+    // TODO: get a better handle on how theme variables are registered to avoid this wonkiness...
+    $params = $title; 
+?>
 <div class="progression_counter">
-    <p>Title: <?php print $title; ?></p>
-    <p>Type: <?php print $counterType; ?></p>
+    <p><i>Title:</i> <?php print $params['title']; ?></p>
+    <p><i>Type:</i> <?php print $params['counterType']; ?></p>
     <p>
-        Target Date: <?php print $targetDate['year'] . '/' 
-                                 . (($targetDate['month'] < 10) ? '0' . $targetDate['month'] : $targetDate['month']) . '/' 
-                                 . (($targetDate['day'] < 10) ? '0' . $targetDate['day'] : $targetDate['day']); ?>
+        <i>Target Date:</i> 
+        <?php print $params['targetDate']['year'] . '/' 
+              . (($params['targetDate']['month'] < 10) ? '0' . $params['targetDate']['month'] : $params['targetDate']['month']) . '/' 
+              . (($params['targetDate']['day'] < 10) ? '0' . $params['targetDate']['day'] : $params['targetDate']['day']); ?>
     </p>
+    <p><i>Description:</i><br/><?php print nl2br(htmlentities($params['description']), TRUE); ?></p>
 </div>
